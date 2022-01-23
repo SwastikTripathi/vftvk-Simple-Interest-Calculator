a77181d487a92b06fdb6bf8a005be8641e9e24ae
SwastikTripathi <88632085+SwastikTripathi@users.noreply.github.com>
2022-01-23 17:09:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    n = document.getElementById("years").value;
+    
+    const onClick = () => {
+        const interest = {p*r*t)/100;
+        var text = document.createTextNode(interest);
+        document.getElementById("result").appendChild(text);
+    }
+};
     
 }
         
